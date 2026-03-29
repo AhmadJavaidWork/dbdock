dc165105a324168a8ae5788bc08cc3704d0deb19
ahmad <ahmadjavaidwork@gmail.com>
2026-03-29 16:57:16 +0500

    added error message helpers

---
+++ frontend/src/errors/connection.error.ts
@@ -0,0 +1,27 @@
+import { DatabaseDriver } from "@/types/database.types";
+import { getErrorMessage } from "@/utils/errorMessagehelper";
+
+export function connectionErrorToText(
+  error: unknown,
+  type: DatabaseDriver,
+  username: string,
+  database: string
+) {
+  const message = getErrorMessage(error);
+  switch (true) {
+    case message.includes("no such host"):
+      return "Host not found. Please check the hostname.";
+
+    case message.includes("connection refused"):
+      return "Connection refused. Check host and port.";
+
+    case message.includes("password authentication failed"):
+      return `Password authentication failed for user ${username}. Please check your password.`;
+
+    case message.includes(`database "${database}" does not exist`):
+      return `Database "${database}" does not exist`;
+
+    default:
+      return message;
+  }
+}
