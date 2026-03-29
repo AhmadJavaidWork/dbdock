25ec34b90c57c6e126f92a1dfb37d5c0229c11cf
ahmad <ahmadjavaidwork@gmail.com>
2026-03-29 16:49:26 +0500

    added layouts

---
+++ frontend/src/layouts/EmptyLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="min-h-screen flex items-center justify-center dark:bg-background-dark text-text-light dark:text-text-dark"
+  >
+    <RouterView />
+  </div>
+</template>
---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex h-screen">
+    <!-- Sidebar -->
+    <aside class="w-60 bg-gray-900 text-white p-4">
+      <h2 class="font-bold mb-4">DBDock</h2>
+      <!-- later: connections list -->
+    </aside>
+
+    <!-- Main content -->
+    <main class="flex-1 p-4 dark:bg-background-dark text-text-light dark:text-text-dark">
+      <RouterView />
+    </main>
+  </div>
+</template>
